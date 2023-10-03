bbdf85de413b8346b8e36a024ab28d6ccd8cdfd1
Hiresh007 <sarojshah5432@gmail.com>
2023-10-03 22:48:14 +0530

    ui changes to authenticate page

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -14,11 +14,17 @@ import { useNavigate } from "react-router-dom";
 
 import { API } from "../../service/api";
 import { DataContext } from "../../context/DataProvider";
-
+import StarsCanvas from "./canvas/Stars";
+import { Tilt } from 'react-tilt'
 const Component = styled(Box)`
   width: 400px;
   margin: auto;
-  box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+  // background: rgba( 255, 255, 255, 0.25 );
+// box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 10px );
+-webkit-backdrop-filter: blur( 8px );
+border-radius: 55px;
+border: 1px solid rgba( 255, 255, 255, 0.85);
 `;
 
 const Image = styled("img")({
@@ -43,19 +49,14 @@ const Wrapper = styled(Box)`
 
 const LoginButton = styled(Button)`
   text-transform: none;
-  background: #fb641b;
   color: #fff;
-  height: 48px;
-  border-radius: 2px;
+
 `;
 
 const SignupButton = styled(Button)`
-  text-transform: none;
-  background: #fff;
-  color: #2874f0;
-  height: 48px;
-  border-radius: 2px;
-  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+text-transform: none;
+color: #fff;
+
 `;
 
 const Text = styled(Typography)`
@@ -147,31 +148,173 @@ const Login = ({ isUserAuthenticated }) => {
   const toggleSignup = () => {
     account === "signup" ? toggleAccount("login") : toggleAccount("signup");
   };
+  const gradientBackground = 'linear-gradient(180deg,#000000 12%,#000000f4 18%,#1c1c1c 30%, #353521f3 50% , #575100d8 70%,#c9bf00 95%)';
+  const styles = {
+    floatingLabelFocusStyle: {
+        color: "white"
+    },
+    input: {
+      color: 'white',
+      fontFamily:'poppin' // Change this to your desired text color
+    },
+}
+
 
   return (
-    <Component>
-      <Box>
-        <Image src={imageURL} alt="Login" />
+    <Box
+      sx={{
+        display:"flex"
+        
+      }}
+    >
+    <Box sx={{
+      background : gradientBackground,
+   
+      width:"100%",
+      height: "100vh",
+      alignItems:"center",
+      justifyContent:"center",
+      display:"flex"
+    }}
+    >
+
+      <StarsCanvas/> 
+    <Component
+     sx={{
+      alignItems:"center",
+        justifyContent:"center",
+        display:"flex",
+        flexDirection:"column",
+          position:"absolute",
+        zIndex: 1,
+     }}
+    >
+      <Box sx={{
+        height:account == "login"?620:670,
+        px:4,
+        alignItems:"center",
+        justifyContent:"center",
+        display:"flex",
+        flexDirection:"column",
+      
+      }}
+      borderRadius={55}   
+      >
+        <Tilt style={{ height: 250, width: 250}} >
+
+        <Image src={imageURL} alt="Login"sx={{
+          height: 250,
+          width:250,
+          borderRadius:"100px",
+          marginTop:"15px",
+          border:"1px solid grey",
+          padding:"5px",
+          opacity:"0.98"
+          
+        }}/>
+        </Tilt>
         {account === "login" ? (
-          <Wrapper>
+          <Wrapper sx={{
+            alignItems:"center",
+        justifyContent:"center",
+        display:"flex",
+        flexDirection:"column",
+          }}>
             <TextField
+                InputProps={{
+                  style:{color:"white", fontFamily:"poppin",borderColor:"whitesmoke",},
+                }}
               onChange={(e) => onValueChange(e)}
               value={login.username}
               name="username"
-              label="Username"
+              label={<Text style={{fontFamily:"poppin", color:"whitesmoke", fontSize:16}}>Username</Text>}
               variant="standard"
-            />
+              style={{width:"240px"}}
+              />
             <TextField
-              onChange={(e) => onValueChange(e)}
-              value={login.password}
+        
+            style={{width:"240px", fontFamily:'poppins', color:"white",fontSize:18,}}
+            onChange={(e) => onValueChange(e)}
+            value={login.password}
+            name="password"
+            type={showPassword ? "text" : "password"} // Toggle password visibility
+            label={<Text style={{fontFamily:"poppin", color:"whitesmoke", fontSize:16}}>Password</Text>}
+            variant="standard"
+            
+            InputProps={{
+              style:{color:"white"},
+              endAdornment: (
+                <InputAdornment position="end">
+                    <IconButton style={{color:"lightgrey"}} onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              />
+
+            {error && <Error>{error}</Error>}
+
+            <LoginButton variant="contained" onClick={() => loginUser()} style={{borderRadius:"20px",fontSize:"18px",fontFamily:"Poppins", width:"180px",height:"50px",background:"#32bf0b"}}>
+              Login
+            </LoginButton>
+            <Text style={{ textAlign: "center" , fontFamily:"Poppins", color:"white", display:'flex',justifyContent:"center", alignItems:"center", fontSize:14,fontWeight:"300"}}>Don't have an account...?
+            <SignupButton
+              
+              onClick={() => toggleSignup()}
+              style={{ color:"black", fontFamily:"poppin",fontWeight:"600",letterSpacing:1.1}}
+              >
+               SignUp
+            </SignupButton>
+             </Text>
+            
+          </Wrapper>
+        ) : (
+          <Wrapper 
+           sx={{
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center"
+           }}
+          >
+            <TextField
+              InputProps={{
+                style:{color:"white", fontFamily:"poppin",borderColor:"whitesmoke",},
+              }}
+              style={{width: 240,}}
+              onChange={(e) => onInputChange(e)}
+              name="name"
+              label={<Text style={{fontFamily:"poppin", color:"whitesmoke", fontSize:16}}>Name</Text>}
+              
+              variant="standard"
+              />
+            <TextField
+              InputProps={{
+                style:{color:"white", fontFamily:"poppin",borderColor:"whitesmoke",},
+              }}
+                
+             floatingLabelFocusStyle={styles.floatingLabelFocusStyle} 
+              InputLabelProps={{
+                
+              }}
+              style={{width:240}}
+              onChange={(e) => onInputChange(e)}
+              name="username"
+              label={<Text style={{fontFamily:"poppin", color:"whitesmoke", fontSize:16}}>Username</Text>}
+              variant="standard"
+              />
+            <TextField
+              style={{width:240}}
+              onChange={(e) => onInputChange(e)}
               name="password"
               type={showPassword ? "text" : "password"} // Toggle password visibility
-              label="Password"
+              label={<Text style={{fontFamily:"poppin", color:"whitesmoke", fontSize:16}}>Password</Text>}
               variant="standard"
               InputProps={{
+                style:{color:"white"},
                 endAdornment: (
                   <InputAdornment position="end">
-                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                     <IconButton style={{color:"lightgrey"}} onClick={() => setShowPassword(!showPassword)}>
                       {showPassword ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
@@ -179,65 +322,27 @@ const Login = ({ isUserAuthenticated }) => {
               }}
             />
 
-            {error && <Error>{error}</Error>}
-
-            <LoginButton variant="contained" onClick={() => loginUser()}>
-              Login
-            </LoginButton>
-            <Text style={{ textAlign: "center" }}>OR</Text>
-            <SignupButton
-              onClick={() => toggleSignup()}
-              style={{ marginBottom: 50 }}
-            >
-              Create an account
+            <SignupButton onClick={() => signupUser()} style={{borderRadius:"20px",fontSize:"18px",fontFamily:"Poppins", width:"180px",height:"50px",background:"#32bf0b"}}>
+              SignUp
             </SignupButton>
-          </Wrapper>
-        ) : (
-          <Wrapper>
-            <TextField
-              onChange={(e) => onInputChange(e)}
-              name="name"
-              label="Name"
-              variant="standard"
-            />
-            <TextField
-              onChange={(e) => onInputChange(e)}
-              name="username"
-              label="Username"
-              variant="standard"
-            />
-            <TextField
-              onChange={(e) => onInputChange(e)}
-              name="password"
-              type={showPassword ? "text" : "password"} // Toggle password visibility
-              label="Password"
-              variant="standard"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
-                    >
-                      {showPassword ? "Hide" : "Show"}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-
-            <SignupButton onClick={() => signupUser()}>
-              Create an Account
-            </SignupButton>
-            <Text style={{ textAlign: "center" }}>
+            <Text style={{ textAlign: "center" , fontFamily:"Poppins", color:"white", display:'flex',justifyContent:"center", alignItems:"center", fontSize:14,fontWeight:"300"}}>
               Already have an account?
-            </Text>
-            <LoginButton onClick={() => toggleSignup()} variant="contained">
+            <LoginButton onClick={() => toggleSignup()} variant="contained"  style={{ color:"black", fontFamily:"poppin",fontWeight:"600",letterSpacing:1.1, background:"none",boxShadow:"none"}}>
               Login
             </LoginButton>
+            </Text>
           </Wrapper>
         )}
       </Box>
     </Component>
+        </Box>
+        <Box sx={{
+          background:"black",
+          height:"100vh"
+        }}>
+     
+        </Box>
+        </Box>
   );
 };
 
